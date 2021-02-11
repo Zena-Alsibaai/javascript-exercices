eab0775aec1b60153e8409308664b0b4b550c518
Zena-Alsibaai <zena.alsibaai@gmail.com>
2021-02-11 12:26:42 +0100

    Finish Dates 05

--- javascript/04-dates/05-get-spooky-fridays/script.js
+++ javascript/04-dates/05-get-spooky-fridays/script.js
@@ -12,5 +12,21 @@
 (function() {
 
     // your code here
+    document.getElementById("run").addEventListener("click", function () {
 
+        function numberOfFridaythe13thsIn(year) {
+            var count = 0;
+            for (var month=0; month<12; month++) {
+                var d = new Date(year,month,13);
+                if(d.getDay() == 5){
+                    var preciseMonth = d.toLocaleString('en-EN', {month: 'long'});
+                    console.log(preciseMonth);
+                    count++;
+               }
+            }
+            return count;
+        }
+
+        alert("There is " + numberOfFridaythe13thsIn(document.getElementById("year").value)  + "Friday the 13th in " + document.getElementById("year").value +". " + "Open the console to find out which ones !");
+    });
 })();
