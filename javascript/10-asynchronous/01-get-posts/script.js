983011709e65d9018b337d926c177fa2ac4936da
Zena-Alsibaai <zena.alsibaai@gmail.com>
2021-02-23 17:13:22 +0100

    Finish async 01

--- javascript/10-asynchronous/01-get-posts/script.js
+++ javascript/10-asynchronous/01-get-posts/script.js
@@ -11,4 +11,21 @@
 
 (() => {
     // your code here
+     // Fonction callback appelée quand on click le button
+    // Je crée la function de callback pour pouvoir l'imbriquer dans la 2eme function
+    // function articles (error, array){
+    //     console.log(error);
+    //     console.log(array);
+    // }
+    document.getElementById("run").addEventListener("click", function(){
+        window.lib.getPosts(function(error, array){
+            
+           if(error) {
+               console.log(error); // on a fait les conditions car parfois ça renvoit error mais pas dans cet exercice
+           }else{
+            console.log(array);
+           }
+            
+        });
+    })
 })();
